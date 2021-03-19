84a79ff67b72ef75c2440bc6ea01e83f0b894997
Maximiliano Vitelli <mvitelli@lanacion.com.ar>
2021-03-19 12:27:29 -0300

    search form, trigger redux, loader

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './Header';
+import Loader from './Loader';
+import SearchForm from './SearchForm';
+import {
+    initiateGetResult,
+} from '../actions/result';
+import { connect } from 'react-redux';
 
-const Dashboard = () => {
-    return <div>Dashboard Page</div>;
+const Dashboard = (props) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [selectedCategory, setSelectedCategory] = useState('albums');
+
+    const handleSearch = (searchTerm) => {
+        setIsLoading(true);
+        props.dispatch(initiateGetResult(searchTerm)).then(() => {
+            setIsLoading(false);
+            setSelectedCategory('albums');
+        });
+    };
+
+    const setCategory = (category) => {
+        setSelectedCategory(category);
+    };
+
+    const { albums, artists, playlist } = props;
+    const result = { albums, artists, playlist };
+
+    return (
+        <div>
+            <Header />
+            <SearchForm handleSearch={handleSearch} />
+            <Loader show={isLoading}>Loading...</Loader>
+        </div>
+    )
+
+
 };
 
-export default Dashboard;
+const mapStateToProps = (state) => {
+    return {
+        albums: state.albums,
+        artists: state.artists,
+        playlist: state.playlist
+    };
+};
+export default connect(mapStateToProps)(Dashboard);
--- src/components/RedirectPage.js
+++ src/components/RedirectPage.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { getParamValues } from '../utils/functions';
 
-const RedirectPage = () => {
-    return <div>Redirect Page</div>;
-};
+const RedirectPage = (props) => {
+    useEffect(() => {
+        const { setExpiryTime, history, location } = props;
+        try {
+            if (!location.hash) {
+                return history.push('/dashboard');
+            }
+            const access_token = getParamValues(location.hash);
+            const expiryTime = new Date().getTime() + access_token.expires_in * 1000;
+            localStorage.setItem('params', JSON.stringify(access_token));
+            localStorage.setItem('expiry_time', expiryTime);
+            history.push('/dashboard');
+        } catch (error) {
+            history.push('/');
+        }
+    }, [])
 
-export default RedirectPage;
+    return (
+        null
+    )
+}
+
+export default RedirectPage
